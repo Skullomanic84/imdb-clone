b8c4aac00bb22e876819c1d4cab5c5f3e3ad8179
Skullomanic84 <jhshiloh@gmail.com>
2023-04-16 21:51:15 +0200

    update about page
    populated content with dummy text

---
+++ src/app/about/page.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <>
+    <div className='max-w-6xl mx-auto space-y-6 p-4'>
+    <h2 className='text-2xl font-medium text-amber-600'>About us</h2>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis odio, recusandae cupiditate quae cumque ratione. Provident placeat debitis iste numquam.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque sequi accusantium blanditiis, ullam nostrum rerum consequuntur odit harum quod ratione eaque voluptatem reiciendis dolore vel temporibus aut sunt delectus obcaecati numquam? Error nobis nesciunt, reiciendis animi aut adipisci aperiam quia asperiores laudantium sequi quidem iusto nisi enim quas possimus hic ab ipsa magni debitis porro eveniet aliquam atque corrupti soluta.</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis, similique obcaecati? Velit quaerat, voluptates, nostrum molestias maiores earum dolorum doloribus dolorem, quidem reprehenderit nulla error at nam dolor iste? Nesciunt corporis aspernatur dolore saepe eius aut obcaecati exercitationem non deleniti ea, at adipisci nobis provident fugit nemo earum aliquid ipsam.</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum perspiciatis veniam, nulla consequuntur fugiat non minima expedita, quidem dolorem quisquam recusandae adipisci rem eaque, corporis deserunt? Quidem quaerat suscipit, ab animi ut modi. Est, delectus facilis possimus tenetur ipsam veniam earum odio sapiente cumque voluptatum quos necessitatibus facere nobis ea, enim doloremque eligendi unde molestiae! Natus atque, officiis maxime possimus ut deleniti molestiae eius architecto nemo esse blanditiis, maiores aliquam illo! Delectus quos magnam nam ratione. Deleniti doloribus animi obcaecati eum ipsa cumque adipisci exercitationem aperiam magni unde, maiores iure sed atque esse perferendis, odit quis nostrum vitae quia velit?</p>
+    </div>
+    </>
+  )
+}
+
+export default About
